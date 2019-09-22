c8fad62b3d1dbb15ffec89fdbda427c83ef881d6
Robert Templeton <roberttee1993@googlemail.com>
2019-09-22 18:08:51 +0100

    removed function from commands

--- commands/berts-mix.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'berts-mix',
-    description: 'Get Roberts playlist.',
-    execute(message, args) {
-        message.channel.send('https://www.youtube.com/playlist?list=PLRvGixglzpD1pltZXoaZrl-kEcYpPdUFK');
-    },
-};
