00a23219f1bf0579907b30fafd154e7956bbe6d7
Robert Templeton <roberttee1993@googlemail.com>
2019-09-23 22:15:09 +0100

    added a catch on to youtube js to try an see error on server

--- commands/youtube.js
+++ commands/youtube.js
@@ -13,49 +13,23 @@ module.exports = {
                 return message.reply('please join a voice channel first!');
             }
 
-                try {
-                    voiceChannel.join().then(connection => {
-                                console.log("STARTING STREAM");
-                                const stream = ytdl('https://www.youtube.com/watch?v=RhU9MZ98jxo', {
-                                    filter: 'audioonly'
-                                })
-                                 const dispatcher = connection.playStream(stream);
 
-                                     dispatcher.on('end', () => voiceChannel.leave())
-                                     .on('error', error => {
-                                         console.log(error)
-                                     });
-                            })
-                         } catch (e) {
-                                console.log(e);
-                            }
+            voiceChannel.join().then(connection => {
+                console.log("STARTING STREAM");
+                const stream = ytdl(`${args[0]}`, {
+                    filter: 'audioonly'
+                });
 
+                const dispatcher = connection.playStream(stream);
 
-            // voiceChannel.join().then(connection => {
-            //     console.log("STARTING STREAM");
-            //     const stream = ytdl(`${args[0]}`, {
-            //         filter: 'audioonly'
-            //     });
+                dispatcher.on('end', () => voiceChannel.leave())
+                .on('error', error => {
+                    console.log(error)
+                });
+            })
+            .catch(err => console.log("ERROR", err))
 
-            //     const dispatcher = connection.playStream(stream);
-
-            //     dispatcher.on('end', () => voiceChannel.leave())
-            //     .on('error', error => {
-            //         console.log(error)
-            //     });
-            // });
-
-            // process.on('unhandledRejection', error => console.error('Uncaught Promise Rejection', error));
+            process.on('unhandledRejection', error => console.error('Uncaught Promise Rejection', error));
 
     },
 };
-
-    // try {
-    //     voiceChannel.join().then(connection => {
-    //                 console.log("STARTING STREAM");
-    //                 const stream = ytdl('https://www.youtube.com/watch?v=RhU9MZ98jxo', {
-    //                     filter: 'audioonly'
-    //                 });
-    //             } catch (e) {
-    //                 console.log(e);
-    //             }
