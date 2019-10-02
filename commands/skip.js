6ee29cb1f5c4c4d11b5dd255c0cc43cfe03b410c
Robert Templeton <roberttee1993@googlemail.com>
2019-10-02 18:54:41 +0100

    deployed fix for skip command

--- commands/skip.js
+++ commands/skip.js
@@ -3,6 +3,7 @@ module.exports = {
     description: 'Skip song in queue.',
     cooldown: 5,
     execute(message, args, queue, serverQueue) {
+        serverQueue = queue.get(message.guild.id);
         if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to stop the music!');
         if (!serverQueue) return message.channel.send('There is no song that I could skip!');
         serverQueue.connection.dispatcher.end();
